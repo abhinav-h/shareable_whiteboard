d577f6b05255a3c3165f95d40602f316a924e590
abhinav singh <abhinav.coding3344@gmail.com>
2020-11-03 18:02:04 +0530

    first commit

--- index.js
+++ index.js
@@ -13,22 +13,25 @@ app.get('/', (req, res) =>{
 
 app.get('/:room', (req, res) =>{
 	let addRoomId = req.params.room;
-    console.log(addRoomId);
+    //console.log(addRoomId);
 	res.render('room',{roomId: `${addRoomId}` }); //get id from address bar and send to ejs
 })
 
 io.on('connection', socket =>{
+	
 	//code to disconnect user using socket simple method ('join-room')
 	socket.on('join-room',(roomId, userId) =>{
-		console.log("room Id:- " + roomId,"userId:- "+ userId);    //userId mean new user 
+		//console.log("room Id:- " + roomId,"userId:- "+ userId);    //userId mean new user 
 		socket.join(roomId); 
 		socket.to(roomId).broadcast.emit('user-connected',userId);
 	});
 	socket.on('mouse', mouseMsg);
-
+	socket.on('clear',(roomid)=>{
+		socket.to(roomid).emit('clearing');
+	})
 	function mouseMsg(data, roomId, userId){
 		socket.to(roomId).broadcast.emit('mouse',data);
-		console.log(data);
+		//console.log(data);
 	}
 });		
 
--- public/sketch.js
+++ public/sketch.js
@@ -1,3 +1,5 @@
+
+
 const socket = io('/');  //socket connection
 
 var peer = new Peer(undefined,{   //we undefine this because peer server create it's own user it
@@ -13,36 +15,82 @@ peer.on('open', id =>{
 socket.on('user-connected', (userId) =>{   //userconnected so we now ready to share 
 	    console.log('user ID fetch connection: '+ userId); //video stream
 })
-
+var change=true;
+const board=document.getElementById("board");
 function setup() {
-  createCanvas(400, 400);
+  let can=createCanvas(800,400);
   background(255);
-  let clear1 = select('#clearCanvas');
-  clear1.mousePressed(clearBG);
+  button = createButton('clear');
+  button.position(0, height+80);
+  pen = createButton('pen');
+  pen.position(0, height-40);
+  eraser = createButton('eraser');
+  eraser.position(0, height-80);
+  colorPicker = createColorPicker('#ed225d');
+  colorPicker.position(0, height );
+  slider = createSlider(0, 25, 5,1);
+  slider.position(0, height +40);
+  slider.style('width', '80px');
+  button.parent("board");
+  can.parent("board");
+  colorPicker.parent("board");
+  slider.parent("board");
+  pen.parent("board");
+  eraser.parent("board");
+  button.mousePressed(clearBG);
+  eraser.mousePressed(mk_false);
+  pen.mousePressed(mk_true);
+  /*button.mousePressed(()=>{
+    socket.emit('mouse', ROOM_ID);
+  })*/
   socket.on('mouse', newDrawing);
+  socket.on('clearing',()=>{
+    background(255);
+    //socket.emit("cleared",ROOM_ID);
+  });
+  board.style.visibility = "hidden";
 }
-
+const show = ()=>{
+  if(board.style.visibility == "hidden")board.style.visibility = "visible";
+  else board.style.visibility = "hidden";
+}
 function newDrawing(data){
-	fill(122);
-    ellipse(data.x, data.y, 10); 
+  if(data.x>100){
+  fill(data.c[0],data.c[1],data.c[2]);
+  stroke(data.c[0],data.c[1],data.c[2]);
+  strokeWeight(data.width);
+    line(data.x,data.y,data.px,data.py);
+  } 
 }
 function mouseDragged(){
-	console.log('sending:' + mouseX +','+ mouseY);
+  //console.log('sending:' + mouseX +','+ mouseY);
+  //colorPicker.color=color('green');
 	var data = {
 		x : mouseX,
-		y : mouseY
-	}
-    
-         socket.emit('mouse', data, ROOM_ID);
- 
-	
-	fill(0)
-    ellipse(mouseX, mouseY,10);
-}
-function draw() {
-
+    y : mouseY,
+    px : pmouseX,
+    py : pmouseY,
+    width : slider.value(),
+    c : colorPicker.color().levels
+  }
+  if(change)data.c=colorPicker.color().levels;
+  else data.c=[255,255,255];
+  socket.emit('mouse', data, ROOM_ID);
+  if(mouseX>100){
+  fill(data.c[0],data.c[1],data.c[2]);
+  stroke(data.c[0],data.c[1],data.c[2]);
+  strokeWeight(slider.value());
+    line(mouseX, mouseY,pmouseX, pmouseY);
+  }
 }
 
 function clearBG(){
-	background(255);
+  background(255);
+  socket.emit('clear', ROOM_ID);
+}
+function mk_true(){
+  change=true;
+}
+function mk_false(){
+  change=false;
 }
